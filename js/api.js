6f24740548beda65aad361661a4fd10b71a490e1
Proalex90 <79789689+Proalex90@users.noreply.github.com>
2021-11-05 15:08:52 +0300

    Добавил API

---
+++ js/api.js
@@ -0,0 +1,44 @@
+const getData = (onSuccess, onError) => () => {
+  fetch(
+    'https://24.javascript.pages.academy/kekstagram/data',
+    {
+      method: 'GET',
+      credentials: 'same-origin',
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error(`${response.status} ${response.statusText}`);
+    })
+    .then((data) => {
+      onSuccess(data);
+    })
+    .catch((err) => {
+      onError(err);
+    });
+};
+
+const sendData = (onSuccess, onFail, body) => {
+  fetch(
+    'https://24.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail();
+      }
+    })
+    .catch(() => {
+      onFail();
+    });
+};
+
+export { getData, sendData };
